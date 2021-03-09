e88bb4d12d2e9a91335cfdcbc526005979cafa0d
ThatKingGuy <43090997+ThatKingGuy@users.noreply.github.com>
2021-03-09 10:07:33 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,121 @@
+<?php
+include("header.php");
+?>
+
+<body>
+
+<div>
+    <h1 class="text-center width-full mt-3">Thin.css</h1>
+    <h4 class="text-center width-full">A cool modern front-end framework</h4>
+    <div class="button-group mt-3">
+        <button style="line-height: 2.5em !important;" onclick="location.href='./#get-started';" class="btn btn-hover inline-button w-20 h-5 mr-1">Get Started
+        </button>
+        <button style="line-height: 2.5em !important;" class="btn btn-hover inline-button w-20 h-5">SourceCode
+        </button>
+    </div>
+    <p class="text-center mt-3 mb-20">Release 1.0.5</p>
+</div>
+
+<div class="jumbotron" id="get-started">
+    <div class="grid-container py-4">
+        <div class="grid-item">
+            <i class="fas fa-bolt"></i>
+            <h3 class="title">Faster development</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut cursus elit. Curabitur quam risus, consectetur vitae aliquet ut, mattis sed turpis. Fusce lacus diam, euismod vitae tellus et, vestibulum malesuada eros. Proin malesuada scelerisque pulvinar. Nullam a purus tortor. Nulla vehicula nec ligula quis semper. Mauris maximus viverra nulla.</p>
+        </div>
+        <div class="grid-item">
+            <i class="fas fa-user-friends"></i>
+            <h3 class="title">User Focused</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eros lacus, eleifend eget lacus a, placerat tincidunt nisi. Nulla in malesuada diam. Integer ligula felis, pellentesque at tempus at, vulputate in turpis. Nam nec ligula odio. Donec sit amet metus quis tellus dapibus porta. Donec tempor fringilla diam, a venenatis.</p>
+        </div>
+        <div class="grid-item">
+            <i class="fas fa-cog"></i>
+            <h3 class="title">Easy to work with</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vulputate sem posuere velit porta, eget suscipit velit ultrices. In ut eleifend mi. Maecenas velit neque, vulputate vel enim et, fringilla iaculis leo. Duis non bibendum mi, non gravida augue. Etiam pharetra convallis ligula, vitae pharetra nunc. Curabitur molestie, mi nec.</p>
+        </div>
+    </div>
+    <hr>
+    <h2 class="mt-2 text-center width-full title">Showcase</h2>
+    <p style="display: block; overflow: auto;
+    width: 50%; max-width: 50%;
+    margin: 0 auto; " class="text-center mb-5">Checkout what people are creating with Materialize. Get inspired by these
+        beautiful sites and you can even submit your own website to be showcased here.
+    </p><br><br>
+    <button style="display: block; margin: 0 auto; " class="btn btn-hover w-24">Explore our Showcase</button>
+    <hr class="mt-4">
+    <h2 class="mt-2 text-center width-full title">Themes</h2>
+    <p style="display: block; overflow: auto;
+    width: 50%; max-width: 50%;
+    margin: 0 auto; " class="text-center mb-5">Take a look at our official themes for Materialize.
+    </p><br><br>
+    <img style="display: block; overflow: auto;
+    margin: 0 auto; " width="27%" height="auto" src="img/theme.png" alt="Themes"><br>
+    <button style="display: block; margin: 0 auto; " class="btn btn-hover w-24">Explore our themes</button>
+    <hr class="mt-4">
+
+    <div class="grid-container py-4">
+        <div class="grid-item">
+            <h3 class="title">Help Materialize Grow</h3>
+            <p class="px-5 text-center mb-2">We hope you have enjoyed using Materialize and if you feel like it has
+                helped you out and want to support the team you can help us by donating or backing us on Patreon. Any
+                amount would help support and continue development on this project and is greatly appreciated.</p>
+            <button style="display: block; margin: 0 auto; " class="btn btn-hover">Support Us</button>
+        </div>
+        <div class="grid-item">
+            <h3 class="title">Join the Discussion</h3>
+            <p class="px-5 text-center mb-2 ">We have a Gitter chat room set up where you can talk directly with us. Come
+                in and discuss new features, future goals, general problems or questions, or anything else you can think
+                of.</p>
+            <button style="display: block; margin: 0 auto; " class="btn btn-hover">Chat</button>
+        </div>
+    </div>
+    <p style="text-align: left; display: inline; width: 50%" class="ml-5">© 2021 Thin.CSS, All rights reserved.</p>
+</div>
+</div>
+
+
+<!--<h3 style="padding-top: 7px">Thin.CSS</h3>
+<h1>Header Size 1</h1>
+<h2>Header Size 2</h2>
+<h3>Header Size 3</h3>
+<h4>Header Size 4</h4>
+<p style="margin-bottom: 27px">This is a paragraph.</p>
+<button class="btn btn-hover" >DOWNLOAD</button> <br><br><br>
+<button class="btn-disabled" disabled>Disabled</button>
+<br>
+<br>
+<br>
+<div class="jumbotron">
+    <h1 class="jumbotron-center">Header</h1>
+    <p>hi</p>
+</div>
+<br>
+<br>
+<br>
+<br>
+<h3>Example Form:</h3>
+<form>
+    <ul class="form-style-1">
+        <li><label>Full Name <span class="required">*</span></label><input type="text" name="field1" class="field-divided" placeholder="First" /> <input type="text" name="field2" class="field-divided" placeholder="Last" /></li>
+        <li>
+            <label>Email <span class="required">*</span></label>
+            <input type="email" name="field3" class="field-long" />
+        </li>
+        <li>
+            <label>Subject</label>
+            <select name="field4" class="field-select">
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
+            </select>
+        </li>
+        <li>
+            <label>Your Message <span class="required">*</span></label>
+            <textarea name="field5" id="field5" class="field-long field-textarea"></textarea>
+        </li>
+        <li>
+            <input type="submit" value="Submit" />
+        </li>
+    </ul>
+</form>-->
+</body>
